be08fe47b61e302d358dcebfa092ec91ace13809
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-02-16 21:22:24 +0600

    Tailwind installed and container configured

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "*.html"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+  },
+  plugins: [],
+}
